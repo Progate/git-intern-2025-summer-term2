de5a68332f3bcd0241c3f9c7c2b48a049a83a144
harune-pg <makkino7086@gmail.com>
2025-08-25 15:58:25 +0900

    refactor: clean up mygit function by removing commented code

--- src/mygit.ts
+++ src/mygit.ts
@@ -1,13 +1,10 @@
-// import { hello } from "./hello.js";
+import { hello } from "./hello.js";
 import { log } from "./log.js";
 
 export const mygit = async (argv: Array<string>): Promise<void> => {
-  // console.log(hello());
-  // console.log(argv);
-
-  // // Avoid eslint error by adding some async operation.
-  // await new Promise((resolve) => setTimeout(resolve, 1000));
   if (argv[2] === "log") {
     await log();
+  } else {
+    console.log(hello());
   }
 };
